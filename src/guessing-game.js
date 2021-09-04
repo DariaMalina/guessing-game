b404433aba3755d59face218681d03d84f83d955
Daria Malina <gluto68@gmail.com>
2021-09-04 22:27:14 +0300

    feat: add and complete a task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    minimum;
+    maximum;
+    number;
+
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.minimum = min
+        this.maximum = max
+        return max + min
     }
 
     guess() {
-
+        this.number = Math.round(this.setRange(this.minimum, this.maximum) / 2)
+        return this.number
     }
 
     lower() {
-
+        this.maximum = this.number
     }
 
     greater() {
-
+        this.minimum = this.number
     }
 }
 
